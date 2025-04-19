8afe2adae4812db3bb69bf7aaff6540a0b4e9f14
hserykava <hserykava@ucsd.edu>
2025-04-18 22:23:43 -0700

    Update global.js

--- global.js
+++ global.js
@@ -29,6 +29,16 @@ for (let p of pages) {
   if (!url.startsWith('http')) {
     url = BASE_PATH + url;
   }
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+  if (a.host === location.host && a.pathname === location.pathname) {
+    a.classList.add('current');
+  }
+  if (a.host !== location.host) {
+    a.target = '_blank';
+  }
+  nav.append(a);
+}
 
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 }
