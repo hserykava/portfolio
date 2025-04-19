facfa810a560f48d74813153ddb10afa0b21b720
hserykava <hserykava@ucsd.edu>
2025-04-18 21:39:07 -0700

    Update global.js

--- global.js
+++ global.js
@@ -8,3 +8,22 @@ let currentLink = navLinks.find(
   (a) => a.host === location.host && a.pathname === location.pathname
 );
 currentLink?.classList.add('current');
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume.html', title: 'Resume' },
+  { url: 'https://github.com/hseryakava', title: 'GitHub' }
+];
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  if (!url.startsWith('http')) {
+    url = BASE_PATH + url;
+  }
+
+  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+}
