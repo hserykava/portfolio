0d836e17564b9960939adad059ce10dc25bafbb9
hserykava <hserykava@ucsd.edu>
2025-04-25 18:47:20 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -3,3 +3,7 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+const titleSpan = document.querySelector('.projects-title');
+if (titleSpan) {
+  titleSpan.textContent = projects.length;
+}
