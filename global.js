53b9ccbac08450e4c74afbfe040882947330990f
hserykava <hserykava@ucsd.edu>
2025-04-25 18:03:32 -0700

    Update global.js

--- global.js
+++ global.js
@@ -66,3 +66,15 @@ select.addEventListener('input', function (event) {
   localStorage.colorScheme = value;
   document.documentElement.style.setProperty('color-scheme', value);
 });
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
