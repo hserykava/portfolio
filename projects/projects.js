31e331c1c39ac738d523521df4bf2f12b94386ce
hserykava <hserykava@ucsd.edu>
2025-05-02 21:49:40 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -9,14 +9,21 @@ const titleSpan = document.querySelector('.projects-title');
 if (titleSpan) {
   titleSpan.textContent = projects.length;
 }
-let data = [1, 2, 3, 4, 5, 5];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' }
+];
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let pie = d3.pie();
-let arcData = pie(data);
+let sliceGenerator = d3.pie().value(d => d.value);
+let arcData = sliceGenerator(data);
 
 let arcs = arcData.map(d => arcGenerator(d));
 
