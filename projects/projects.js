1a9e9eb8718bd43d2e12238293c65ed910ea3c53
hserykava <hserykava@ucsd.edu>
2025-05-02 22:28:03 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -9,34 +9,36 @@ const titleSpan = document.querySelector('.projects-title');
 if (titleSpan) {
   titleSpan.textContent = projects.length;
 }
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' }
-];
 
-let colors = d3.scaleOrdinal(d3.schemeTableau10);
+let rolledData = d3.rollups(
+  projects,
+  v => v.length,
+  d => d.year
+);
 
-let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+let data = rolledData.map(([year, count]) => {
+  return { label: year, value: count };
+});
 
 let sliceGenerator = d3.pie().value(d => d.value);
 let arcData = sliceGenerator(data);
-
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 let arcs = arcData.map(d => arcGenerator(d));
 
-arcs.forEach((arc, idx) => {
-  d3.select('svg')
-    .append('path')
-    .attr('d', arc)
-    .attr('fill', colors(idx));
-});
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
+
+d3.select('svg')
+  .selectAll('path')
+  .data(arcs)
+  .enter()
+  .append('path')
+  .attr('d', d => d)
+  .attr('fill', (_, i) => colors(i));
+
 let legend = d3.select('.legend');
-data.forEach((d, idx) => {
+arcData.forEach((d, i) => {
   legend.append('li')
-    .attr('style', `--color:${colors(idx)}`)  // applies the slice color
-    .attr('class', 'legend-item')            // optional: useful for styling each <li>
-    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
+    .attr('style', `--color:${colors(i)}`)
+    .attr('class', 'legend-item')
+    .html(`<span class="swatch"></span> ${d.data.label} <em>(${d.data.value})</em>`);
 });
