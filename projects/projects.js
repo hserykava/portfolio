4e27a87194f37177be77acc4596a0d3282005594
hserykava <hserykava@ucsd.edu>
2025-05-02 20:55:20 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,3 +1,4 @@
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
