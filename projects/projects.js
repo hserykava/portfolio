91c29220960776fb50df40a909f7bb281df95129
hserykava <hserykava@ucsd.edu>
2025-05-02 21:21:53 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -2,23 +2,32 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
-
 renderProjects(projects, projectsContainer, 'h2');
 const titleSpan = document.querySelector('.projects-title');
 if (titleSpan) {
   titleSpan.textContent = projects.length;
 }
+
+const data = [1, 2];
 const arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
 
-const arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
+let total = data.reduce((sum, d) => sum + d, 0);
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle: endAngle });
+  angle = endAngle;
+}
+
+const arcs = arcData.map(d => arcGenerator(d));
+const colors = ['gold', 'purple'];
+arcs.forEach((arc, idx) => {
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
 });
-
-d3.select('svg')
-  .append('path')
-  .attr('d', arc)
-  .attr('fill', 'red');
-
