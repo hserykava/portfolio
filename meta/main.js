8a48e6ef353d4f7902b8c674248bb956b84a17cb
hserykava <hserykava@ucsd.edu>
2025-05-10 18:11:57 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -1,10 +1,11 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
+
 async function loadData() {
   const data = await d3.csv('loc.csv', (row) => ({
     ...row,
-    line: Number(row.line),
-    depth: Number(row.depth),
-    length: Number(row.length),
+    line: +row.line,
+    depth: +row.depth,
+    length: +row.length,
     date: new Date(row.date + 'T00:00:00' + row.timezone),
     datetime: new Date(row.datetime),
   }));
@@ -16,10 +17,9 @@ function processCommits(data) {
     .map(([commit, lines]) => {
       const first = lines[0];
       const { author, date, time, timezone, datetime } = first;
-
       const ret = {
         id: commit,
-        url: 'https://github.com/YOUR-USERNAME/YOUR-REPO/commit/' + commit,
+        url: 'https://github.com/YOUR_USERNAME/YOUR_REPO/commit/' + commit,
         author,
         date,
         time,
@@ -28,35 +28,23 @@ function processCommits(data) {
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
         totalLines: lines.length,
       };
-
-      Object.defineProperty(ret, 'lines', {
-        value: lines,
-        configurable: true,
-        writable: true,
-        enumerable: false
-      });
-
+      Object.defineProperty(ret, 'lines', { value: lines, enumerable: false });
       return ret;
     });
 }
 
 function renderCommitInfo(data, commits) {
-  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
+  const dl = d3.select('#stats')
+    .append('dl')
+    .attr('class', 'stats');
 
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
 
-
   dl.append('dt').text('Total commits');
   dl.append('dd').text(commits.length);
-
 }
 
-async function main() {
-  const data = await loadData();
-  const commits = processCommits(data);
-
-  renderCommitInfo(data, commits);
-}
-
-main();
+const data = await loadData();
+const commits = processCommits(data);
+renderCommitInfo(data, commits);
