9cde56f9650a7b53771157037f11e1cd8bc5578a
hserykava <hserykava@ucsd.edu>
2025-05-10 18:52:44 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -62,20 +62,30 @@ function renderScatterPlot(data, commits) {
   const width = 1000;
   const height = 600;
 
+  const margin = { top: 10, right: 10, bottom: 30, left: 20 };
+
+  const usableArea = {
+    top: margin.top,
+    right: width - margin.right,
+    bottom: height - margin.bottom,
+    left: margin.left,
+    width: width - margin.left - margin.right,
+    height: height - margin.top - margin.bottom
+  };
+
   const svg = d3.select('#chart')
     .append('svg')
     .attr('viewBox', `0 0 ${width} ${height}`)
     .style('overflow', 'visible');
 
-
   const xScale = d3.scaleTime()
     .domain(d3.extent(commits, d => d.datetime))
-    .range([0, width])
+    .range([usableArea.left, usableArea.right])
     .nice();
 
   const yScale = d3.scaleLinear()
     .domain([0, 24])
-    .range([height, 0]);
+    .range([usableArea.bottom, usableArea.top]);
 
   const dots = svg.append('g')
     .attr('class', 'dots');
@@ -87,6 +97,18 @@ function renderScatterPlot(data, commits) {
     .attr('cy', d => yScale(d.hourFrac))
     .attr('r', 5)
     .attr('fill', 'steelblue');
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat(d => String(d % 24).padStart(2, '0') + ':00'); 
+
+  svg.append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
 }
 const data = await loadData();
 const commits = processCommits(data);
