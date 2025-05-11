ef601dfb9bd738786017cbd1a1d373f3a7e9da7c
hserykava <hserykava@ucsd.edu>
2025-05-10 17:48:07 -0700

    Update main.js

--- meta/main.js
+++ meta/main.js
@@ -1,22 +1,25 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-
 async function loadData() {
-  const data = await d3.csv('loc.csv');
-  console.log(data);
+  const data = await d3.csv('loc.csv', (row) => ({
+    ...row,
+    line: Number(row.line),
+    depth: Number(row.depth),
+    length: Number(row.length),
+    date: new Date(row.date + 'T00:00:00' + row.timezone),
+    datetime: new Date(row.datetime),
+  }));
   return data;
 }
-
 
 function processCommits(data) {
   return d3.groups(data, d => d.commit)
     .map(([commit, lines]) => {
-      let first = lines[0]; 
-      
-      let { author, date, time, timezone, datetime } = first;
-      
-      let ret = {
+      const first = lines[0];
+      const { author, date, time, timezone, datetime } = first;
+
+      const ret = {
         id: commit,
-        url: 'https://github.com/hserykava/portfolio/commit/' + commit,
+        url: 'https://github.com/YOUR-USERNAME/YOUR-REPO/commit/' + commit,
         author,
         date,
         time,
@@ -25,8 +28,7 @@ function processCommits(data) {
         hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
         totalLines: lines.length,
       };
-      
-  
+
       Object.defineProperty(ret, 'lines', {
         value: lines,
         configurable: true,
@@ -39,9 +41,7 @@ function processCommits(data) {
 }
 
 function renderCommitInfo(data, commits) {
-  const dl = d3.select('#stats')
-    .append('dl')
-    .attr('class', 'stats');
+  const dl = d3.select('#stats').append('dl').attr('class', 'stats');
 
   dl.append('dt').html('Total <abbr title="Lines of code">LOC</abbr>');
   dl.append('dd').text(data.length);
@@ -51,7 +51,12 @@ function renderCommitInfo(data, commits) {
   dl.append('dd').text(commits.length);
 
 }
-let data = await loadData();
-let commits = processCommits(data);
 
-renderCommitInfo(data, commits);
+async function main() {
+  const data = await loadData();
+  const commits = processCommits(data);
+
+  renderCommitInfo(data, commits);
+}
+
+main();
